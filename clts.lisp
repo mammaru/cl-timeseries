5d62afd763f121623c84623d5553ac1b53d4e277
mammaru <mauma1989@gmail.com>
2015-07-28 13:34:56 +0900

    define class var and ssm

--- clts.lisp
+++ clts.lisp
@@ -1,1 +1,25 @@
 (in-package :clts)
+
+(defclass vector-auto-regressive-model ()
+  (dimention
+   :initarg :dimention
+   :initform 1)
+  (A ;;transition matrix
+   :initarg :A
+   :initform 1)
+  (E ;;error matrix
+   :initarg :E
+   :initform 1)) 
+
+(defclass state-space-model ()
+  (observation-dimention
+   :initarg :obs-dim
+   :initform (error "Must supply dimention of observation as :obs-dim"))
+  (system-dimention
+   :initarg :sys-dim
+   :initform (error "Must supply dimention of system as :sys-dim"))
+  (initial-mean-of-system
+   :initarg :x0mean)
+  (initial-variance-of-system
+   :initform :x0var)
+  )
